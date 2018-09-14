abbf452a37c4ff7ebffd62a1cf79078865ed7322
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-14 11:51:04 -0300

    Exercicios -

---
+++ 05-Spring-MVC-JPA/WebContent/WEB-INF/jsp/home/index.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="tags"  tagdir="/WEB-INF/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<tags:template titulo="Home">
+	<h1>Bem-vindo</h1>
+</tags:template>
---
+++ 05-Spring-MVC-JPA/src/br/com/fiap/spring/model/Partida.java
@@ -0,0 +1,109 @@
+package br.com.fiap.spring.model;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+@SequenceGenerator(name="partida",allocationSize=1)
+public class Partida {
+
+	@Id
+	@GeneratedValue(generator="partida", strategy=GenerationType.SEQUENCE)
+	private int codigo;
+	
+	@Column(name="qt_duracao")
+	private int duracao;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="dt_partida")
+	private Calendar data;
+	
+	private String timeA;
+	
+	private String timeB;
+	
+	private int golsTimeA;
+	
+	private int golsTimeB;
+	
+	
+	private boolean realizado;
+
+	
+	
+	//GETTER'S AND SETTER'S
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public int getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(int duracao) {
+		this.duracao = duracao;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	public String getTimeA() {
+		return timeA;
+	}
+
+	public void setTimeA(String timeA) {
+		this.timeA = timeA;
+	}
+
+	public String getTimeB() {
+		return timeB;
+	}
+
+	public void setTimeB(String timeB) {
+		this.timeB = timeB;
+	}
+
+	public int getGolsTimeA() {
+		return golsTimeA;
+	}
+
+	public void setGolsTimeA(int golsTimeA) {
+		this.golsTimeA = golsTimeA;
+	}
+
+	public int getGolsTimeB() {
+		return golsTimeB;
+	}
+
+	public void setGolsTimeB(int golsTimeB) {
+		this.golsTimeB = golsTimeB;
+	}
+
+	public boolean isRealizado() {
+		return realizado;
+	}
+
+	public void setRealizado(boolean realizado) {
+		this.realizado = realizado;
+	}
+	
+	
+}
